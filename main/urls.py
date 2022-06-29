1166479cc024d98fd621ce5749fcef2459d8577f
Filipe Vasconcelos <filipev@me.com>
2022-06-29 07:03:40 -0300

    Save urls.py.

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomeView, DrinkListView, DrinkCreateView, PubCreateView, PubListView
+from .views import HomeView, DrinkListView, DrinkCreateView, PubCreateView, PubListView, DrinkUpdateView, DrinkDeleteView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
