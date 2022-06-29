5100107589b7dc8dd6d9759db9fa169e22459871
Filipe Vasconcelos <filipev@me.com>
2022-06-29 07:05:43 -0300

    Save urls.py.

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('drinks/', DrinkListView.as_view(), name='drinks'),
     path('drinks/new/', DrinkCreateView.as_view(), name='drink.new'),
     path('drinks/<int:pk>/edit/', DrinkUpdateView.as_view(), name='drink.edit'),
+    path('drinks/delete/<int:pk>/', DrinkListView.as_view(), name='drink.delete'),
     path('pubs/', PubListView.as_view(), name='pubs'),
     path('pubs/new/', PubCreateView.as_view(), name='new_pub'),
 ]
