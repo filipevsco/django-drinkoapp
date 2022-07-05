7ff46404b64ceadcefa6bf0b606ea14b4152c8ab
Filipe Vasconcelos <filipev@me.com>
2022-07-05 06:40:02 -0300

    Save urls.py.

--- main/urls.py
+++ main/urls.py
@@ -1,16 +1,15 @@
 from django.urls import path
-from .views import HomeView, DrinkListView, DrinkCreateView, DrinkUpdateView, DrinkDeleteView, PubListView, PubCreateView, PubUpdateView, PubDeleteView, PubDetailView, DrinkDetailView
-
+from . import views
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('drinks/', DrinkListView.as_view(), name='drinks'),
-    path('drinks/detail/<int:pk>/', DrinkDetailView.as_view(), name="drink.detail"),
-    path('drinks/new/', DrinkCreateView.as_view(), name='drink.new'),
-    path('drinks/edit/<int:pk>/', DrinkUpdateView.as_view(), name='drink.edit'),
-    path('drinks/delete/<int:pk>/', DrinkDeleteView.as_view(), name='drink.delete'),
-    path('pubs/', PubListView.as_view(), name='pubs'),
-    path('pubs/detail/<int:pk>/', PubDetailView.as_view(), name="pub.detail"),
-    path('pubs/new/', PubCreateView.as_view(), name='pub.new'),
-    path('pubs/edit/<int:pk>/', PubUpdateView.as_view(), name='pub.edit'),
-    path('pubs/delete/<int:pk>/', PubDeleteView.as_view(), name='pub.delete'),
+    path('', views.HomeView.as_view(), name='home'),
+    path('drinks/', views.DrinkListView.as_view(), name='drinks'),
+    path('drinks/detail/<int:pk>/', views.DrinkDetailView.as_view(), name="drink.detail"),
+    path('drinks/new/', views.DrinkCreateView.as_view(), name='drink.new'),
+    path('drinks/edit/<int:pk>/', views.DrinkUpdateView.as_view(), name='drink.edit'),
+    path('drinks/delete/<int:pk>/', views.DrinkDeleteView.as_view(), name='drink.delete'),
+    path('pubs/', views.PubListView.as_view(), name='pubs'),
+    path('pubs/detail/<int:pk>/', views.PubDetailView.as_view(), name="pub.detail"),
+    path('pubs/new/', views.PubCreateView.as_view(), name='pub.new'),
+    path('pubs/edit/<int:pk>/', views.PubUpdateView.as_view(), name='pub.edit'),
+    path('pubs/delete/<int:pk>/', views.PubDeleteView.as_view(), name='pub.delete'),
 ]
