71a79c004d4bda46df3322febe299670101ef795
Filipe Vasconcelos <filipev@me.com>
2022-07-06 07:30:36 -0300

    Save models.py.

--- users/models.py
+++ users/models.py
@@ -14,6 +14,10 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
         
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+        
 
 class CustomUser(AbstractUser):
     pass
