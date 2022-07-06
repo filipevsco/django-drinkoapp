7ba0f310c8d6c69fc6e09901e5be6420c340f6a1
Filipe Vasconcelos <filipev@me.com>
2022-07-06 12:52:03 -0300

    Save admin.py.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+
+from .forms import CustomUserCreateForm, CustomUserChangeFotm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreateForm
+    form = CustomUserChangeFotm
+    model = CustomUser
+    list_display = ["first_name", "last_name", "email", "is_staff"]
+    fieldsets = {
+        (None, {"fields" : ("email": "password")})
+        ("Informações Pessoas", {"fields": ("is_active", "is_staff", "is_superuser", "groups")})
+    }
