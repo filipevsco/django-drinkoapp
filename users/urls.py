ef9fac39fe6155b8515778bccacfc43e0b476c75
Filipe Vasconcelos <filipev@me.com>
2022-07-06 06:14:20 -0300

    Save urls.py.

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,6 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
+from django.urls import path, include
+
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name="login.html"), name="login"),
-    path('logout/', auth_views.LogoutView.as_view(), name="logout"),
+    path('', include("django.contrib.auth.urls"),
     ]
