5c2d55c3c1b2baec4529a46b87632c38e6cd6633
Filipe Vasconcelos <filipev@me.com>
2022-07-06 07:35:03 -0300

    Save models.py.

--- users/models.py
+++ users/models.py
@@ -18,6 +18,12 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault("is_superuser", False)
         return self._create_user(email, password, **extra_fields)
         
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_superuser", True)
+        
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser needs to be True")
+            
 
 class CustomUser(AbstractUser):
     pass
